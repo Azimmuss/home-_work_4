fa76f2fa698ca6a9d4edddaa4dbbc444474442f5
Azimmuss <Azimus076@gmail.com>
2025-12-09 11:50:46 +0600

    hw3

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -9,3 +10,25 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+def validate_rating(value):
+    if value < 1 or value > 5:
+        raise ValidationError("Ставьте оценку только от 1 до 5")
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+
+    rating = models.IntegerField(validators=[validate_rating])
+
+    text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        if len(self.text) < 200:
+            raise ValidationError("Текст отзыва должен быть минимум 200 символов")
+
+    def __str__(self):
+        return f"Review for {self.book.title}"
