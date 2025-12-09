5337668c823f2ffc438a90ddcf984d01a5d72ff3
Azimmuss <Azimus076@gmail.com>
2025-12-09 15:28:32 +0600

    hw4

--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404  
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from datetime import datetime
 from .models import Book  
+from .forms import BookForm
 
 
 def helloWordViews(request):
@@ -45,3 +46,34 @@ def book_list(request):
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, "books/book_detail.html", {"book": book})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+
+    return render(request, 'books/book_form.html', {'form': form})
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+
+    if form.is_valid():
+        form.save()
+        return redirect('book_detail', pk=pk)
+
+    return render(request, 'books/book_form.html', {'form': form})
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+
+    return render(request, 'books/book_delete_confirm.html', {'book': book})
+
